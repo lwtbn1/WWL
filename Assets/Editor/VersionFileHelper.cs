1aad3cdec334179a2ac6f2e0c9370f66efe8f75b
lwtbn1 <lwtbn1@126.com>
2015-07-24 00:29:11 +0800

    首次提交
    一个名叫WWL的unity3d热更新游戏框架诞生了！

---
+++ Assets/Editor/VersionFileHelper.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using LitJson;
+using System.Security.Cryptography;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using LitJson;
+public class VersionFileHelper
+{
+    public static MD5 md5 = MD5.Create();
+    [MenuItem("Custom Editor/Create Version File")]
+    public static void CreateVersionFile()
+    {
+        List<string> allAssetBundleFullPath = new List<string>();
+        List<Define.VersionInfo> versionInfoList = new List<Define.VersionInfo>();
+        FileHelper.GetAllFileFullPath(Define.StreamingAssetsRoot, ref allAssetBundleFullPath, ".unity3d");
+        for (int i = 0; i < allAssetBundleFullPath.Count; i++)
+        {
+            string fullName = allAssetBundleFullPath[i];
+            FileInfo fileInfo = new FileInfo(fullName);
+            if (!fileInfo.Exists)
+                continue;
+            byte[] bytes = FileHelper.ReadBytes(fullName);
+            byte[] hashBytes = md5.ComputeHash(bytes);
+            StringBuilder sBuilder = new StringBuilder();
+            for (int j = 0; j < hashBytes.Length; j++)
+            {
+                sBuilder.Append(hashBytes[j].ToString("x2"));
+            }
+            string fileName = fullName.Substring(fullName.IndexOf("StreamingAssets") + "StreamingAssets".Length + 1, fullName.Length - fullName.IndexOf("StreamingAssets") - "StreamingAssets".Length - 1);
+            Define.VersionInfo versionInfo = new Define.VersionInfo();
+            versionInfo.fileName = fileName;
+            versionInfo.md5 = sBuilder.ToString();
+            versionInfoList.Add(versionInfo);
+        }
+        string jsonStr = JsonMapper.ToJson(versionInfoList);
+        string versionFile = Define.StreamingAssetsRootByPlatForm + "version.dat";
+
+        FileStream fs = null;
+        if (!File.Exists(versionFile))
+            fs = new FileStream(versionFile, FileMode.Create, FileAccess.Write);//创建写入文件
+        else
+            fs = new FileStream(versionFile, FileMode.Open, FileAccess.Write);  //打开写入文件
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine(jsonStr);//开始写入值
+        sw.Close();
+        fs.Close();
+        AssetDatabase.Refresh();
+    }
+
+    
+}
+
+
+
+
